c87b1145a7f748e1fb6210ef951107dc32c7627a
Phalcorine <phalcorine@gmail.com>
2020-07-28 07:22:43 +0100

    [Client] Added StringUtitlities Helper class

---
+++ BlazorMovies/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string ToUpper(string value) => value.ToUpper();
+        public static string ToLower(string value) => value.ToLower();
+    }
+}
