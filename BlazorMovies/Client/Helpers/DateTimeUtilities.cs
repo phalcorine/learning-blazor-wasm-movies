8e99591a34aa4473b43adf017d384963733e93dc
Phalcorine <phalcorine@gmail.com>
2020-07-28 11:16:40 +0100

    [Client] Added async behaviour to RazorTest component...

---
+++ BlazorMovies/Client/Helpers/DateTimeUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class DateTimeUtilities
+    {
+        public const string NORMALIZED_DATE_FORMAT = "dd MMM yyyy";
+    }
+}
---
+++ BlazorMovies/Shared/Domain/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Domain.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleasedDate { get; set; }
+
+        public Movie()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
