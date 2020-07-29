54460063c904361832e952e32c2c97af08a36445
Phalcorine <phalcorine@gmail.com>
2020-07-29 10:50:24 +0100

    [Shared] Implemented Movie Repository service

---
+++ BlazorMovies/Shared/Domain/Repositories/MovieRepository.cs
@@ -0,0 +1,35 @@
+﻿using BlazorMovies.Shared.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Domain.Repositories
+{
+    public class MovieRepository : ICrudRepository<Movie>
+    {
+        public IList<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie()
+                {
+                    Title = "Mad O!!!",
+                    Description = "A mad movie... ",
+                    ReleasedDate = new DateTime(2020, 6, 5)
+                },
+                new Movie()
+                {
+                    Title = "Attack on Titan: Chronicles",
+                    Description = "This is the best movie ever",
+                    ReleasedDate = new DateTime(2020, 6, 5)
+                },
+                new Movie()
+                {
+                    Title = "Sonic the Hedgehog",
+                    Description = "This movie has the greatest comeback",
+                    ReleasedDate = new DateTime(2020, 6, 5)
+                },
+            };
+        }
+    }
+}
