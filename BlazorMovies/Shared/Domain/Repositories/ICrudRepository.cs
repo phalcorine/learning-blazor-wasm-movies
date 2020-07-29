695aa48e4643b25e0946d3df4991d7c50db7b727
Phalcorine <phalcorine@gmail.com>
2020-07-29 10:45:10 +0100

    [Shared] Added Crud Repository interface

---
+++ BlazorMovies/Shared/Domain/Repositories/ICrudRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Domain.Repositories
+{
+    interface ICrudRepository<TEntity>
+    {
+        IList<TEntity> GetMovies();
+    }
+}
