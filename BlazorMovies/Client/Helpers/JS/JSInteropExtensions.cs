9a9df9f70168760d40934179d9a7a2df06d05693
Phalcorine <phalcorine@gmail.com>
2020-07-29 12:30:23 +0100

    [Client] Added JSInteropExtension class / methods (Confim, Alert).

---
+++ BlazorMovies/Client/Helpers/JS/JSInteropExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers.JS
+{
+    public static class JSInteropExtensions
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime jSRuntime, string message)
+        {
+            var confirmed = await jSRuntime.InvokeAsync<bool>("confirm", message);
+
+            return confirmed;
+        }
+
+        public static async Task Alert(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("alert", message);
+        }
+    }
+}
